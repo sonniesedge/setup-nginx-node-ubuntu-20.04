d48026e1685d23ae083f33d5162e6ff413734eb8
Charlie Owen <charlie@sonniesedge.co.uk>
2020-10-29 07:36:49 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,7 +21,12 @@ server {
         server_name whalecoiner.com www.whalecoiner.com;
 
         location / {
-                try_files $uri $uri/ =404;
+          proxy_pass http://localhost:3000;
+          proxy_http_version 1.1;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection 'upgrade';
+          proxy_set_header Host $host;
+          proxy_cache_bypass $http_upgrade;
         }
 }
 EOT
@@ -48,3 +53,24 @@ sudo certbot --nginx -d whalecoiner.com -d www.whalecoiner.com
 
 # Renew certs automatically
 sudo systemctl status certbot.timer
+
+sudo systemctl restart nginx
+
+
+# https://www.digitalocean.com/community/tutorials/how-to-set-up-a-node-js-application-for-production-on-ubuntu-20-04
+
+curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
+
+sudo bash nodesource_setup.sh
+
+sudo apt-get install nodejs
+
+sudo apt-get install build-essential
+
+sudo npm install pm2@latest -g
+
+pm2 startup systemd
+
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u MYUSERNAME --hp /home/MYUSERNAME
+
+pm2 save
