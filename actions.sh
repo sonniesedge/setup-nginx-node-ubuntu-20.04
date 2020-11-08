314c34c9970d80a352407b0ea3adf10b19c89ec8
Charlie Owen <charlie@sonniesedge.co.uk>
2020-11-08 17:22:45 +0100

    Update

--- actions.sh
+++ actions.sh
@@ -105,13 +105,7 @@ debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Si
 
 # ------------------------------------------------------------
 log "Install all necessary packages"
-apt-get -qqy install \
-  nginx certbot python3-certbot-nginx \
-  brotli nginx-module-brotli \
-  build-essential libssl-dev \
-  mailutils whois unattended-upgrades \
-  nodejs \
-  /
+apt-get -qqy install nginx certbot python3-certbot-nginx brotli nginx-module-brotli build-essential libssl-dev mailutils whois unattended-upgrades nodejs 
 
 # ------------------------------------------------------------
 log "Setting up unattended upgrades"
