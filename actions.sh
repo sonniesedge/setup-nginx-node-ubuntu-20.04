53bf58e7ae2ce8ab4a7900734a81e8359d6d8311
Charlie Owen <charlie@sonniesedge.co.uk>
2020-11-11 14:33:21 +0100

    Update actions.sh

--- actions.sh
+++ actions.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+
 # ------------------------------------------------------------
 HOSTNAME=whalecoiner
 DOMAINNAME=whalecoiner.com
@@ -12,11 +14,15 @@ DOMAINALIASES_SPACE_SEPARATED="${DOMAINALIASES_SPACE_SEPARATED% }"
 DEPLOYUSER=deploy
 SUDOUSER=charlie
 
+
+
 # ------------------------------------------------------------
 log ()
 {
   echo "$(date "+%m%d%Y %T"): $1" >> $LOG
 }
+
+
 
 log "-------------------------"
 log "| CONFIG"
@@ -28,6 +34,8 @@ log "DOMAINALIASES_SPACE_SEPARATED: $DOMAINALIASES_SPACE_SEPARATED"
 log "DEPLOYUSER: $DEPLOYUSER"
 log "SUDOUSER: $SUDOUSER"
 
+
+
 # -------------------------------
 # CREATE USERS AND CONFIGURE SSH
 # -------------------------------
@@ -37,6 +45,8 @@ cat <<EOT >> /etc/sshd_config
 ClientAliveInterval 120
 ClientAliveCountMax 720
 EOT
+
+
 
 # ------------------------------------------------------------
 log "Creating $DEPLOYUSER"
@@ -53,6 +63,8 @@ cat <<EOT >>/home/$DEPLOYUSER/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCyJ7K96iFzBFADuS71quXKmcoguMhypW8GiEwQ8e16limTbFpQOxl6aGnlHBqjk3FrwtR8k5t3L3e+HzFbF+wpQEjAUHJn8AshJBrQYzT7mFlMx2fUhUU1H6KxrcEwK1TGsUjxWQn2+fLRL0ZAl5zwSfqAVMIQhZcE/7ADZkaShZMfFDFmW3Gtqp+UCCBHfFcGKIQy+fvJguNds67MuRh2fJwxEu0E7iv74wG2O937oUPSUxP36azGJ7l3nZz2smKogVhpiOlKfXm6PoBeqleTRhfw+aoWoZ5LUWeLkr+4gB59p8XsfyJJoW9CtP6MZMtjOlxv/7GYRWlv+bekVM2fF1ek/Csw0EjtgjlQohaWhW4EklnJ5fNtR0NVMR4L9Bn2ll73mbwf+/4Rx/CD+pffdYV0YTws7M/z32qbUc+IHOSbHGeFhnNCMbk8I4rLgm7/sNvtb3uF7S9Y8Ewe012LvCSCPwrVuHMobKGbJt2F7ZeOlk3Uf+oG0iRTEU7Ngmq8EofrMubhSFUjcC4KzhlUSu/fniAsFJPk2zVyPtU205NwdyWEY7+RCyvwwHHV0yMuhbBc99eUUKpBQI94PaZfAicCsLQSVN9ldQ5vvh7CKJrUJRfq2yCmmRG0EhmSjpB8YUcgT9RdM7kD+76BG0MLMv4ThDNnuCLSnMHnTjMP7w==
 EOT
 
+
+
 # ------------------------------------------------------------
 log "Creating $SUDOUSER" >> $LOG
 # Using the low-level 'useradd' rather than the high-level 'adduser' as it allows the supplying of an encrypted password
@@ -63,10 +75,11 @@ touch /home/$SUDOUSER/.ssh/authorized_keys
 chown -R $SUDOUSER:$SUDOUSER /home/$SUDOUSER/.ssh
 chmod 700 /home/$SUDOUSER/.ssh
 chmod 644 /home/$SUDOUSER/.ssh/authorized_keys
-
 cat <<EOT >>/home/$SUDOUSER/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC1SuXSEyv07abIrcbkw/U7uhgJdTsIffiG7XOLIgELG6KphYsIlC/lnoq5P0xj9oZ7W28zX+qIkg/PUSMQS3uJpynBS1y43v51Eac6SzhtH56SrAhMDfVJTclMaUAIMnTb0lN/vhD6w6bsz95AoWwInHja/4J3a1aso0qWwdyrLFZ8Y5vDLokn27EdKuqZAPfzk3VIF+zh1OXnnP3XeeTsAqzVOPdzTc1XlAEokgnmizjgIuXOn6yIMAct24r6TIgwQPBMPjP5pN7gtwY1StZXk62N6s1pm7obXLaJYeHGIHNhKbz3YW9hy23hbBOPA4WD406rICIg2NxF7GXccBAo9V46glpncWtTnBbpmItXXJ842gW+NpuHks2mn3evVFw70KRO2z2H/YmZoCBFXzxNbPquaZPaT7i+u8JrUSQz8Sn3XVgmXSzDIqraJxQtKVKx95MyLd1UTwcMeMf4zmsnfdgBjhIIGS3k8B9QlZxDbYhmhL+/FW14gG7zU7lze0lrgXqbH+5LBHyfyg98GzJKOGj9a6b3bvbAJcSl4PxJIpEISHAh57DkN76UPFT0dloUic2GjJ+sRLr7cdJvoUfCJ5pk7i19jhb8pZM09bG/QEJEkOIFH6ZtkT1miGLZzD5tHt9ORuZlq3aWhP2yRuKsA51gCY+d1KJuj636+LsY9Q==
 EOT
+
+
 
 # ------------------------------------------------------------
 grep -q 'PasswordAuthentication no' /etc/ssh/sshd_config 2>/dev/null
@@ -74,6 +87,8 @@ if [ $? ] >0; then
   log "Disabling SSH password logins"
   sed -i -e 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 fi
+
+
 
 # ------------------------------------------------------------
 grep -q 'PermitRootLogin no' /etc/ssh/sshd_config 2>/dev/null
@@ -83,6 +98,8 @@ if [ $? ] >0; then
   sed -i -e 's/PermitRootLogin without-password/PermitRootLogin no/g' /etc/ssh/sshd_config
 fi
 
+
+
 # ------------------------------------------------------------
 log "Add the node repo to apt..."
 curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
@@ -91,6 +108,8 @@ DISTRO="$(lsb_release -s -c)" # Get the ubuntu distribution name
 echo "deb https://deb.nodesource.com/$NODEVERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 echo "deb-src https://deb.nodesource.com/$NODEVERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
 
+
+
 # ------------------------------------------------------------
 # log "Updating apt..."
 # apt-get -qq update
@@ -98,10 +117,14 @@ echo "deb-src https://deb.nodesource.com/$NODEVERSION $DISTRO main" | sudo tee -
 #   log "apt updated!"
 # fi
 
+
+
 # ------------------------------------------------------------
 # Setup these values before installing mailutils/postfix so that unattended install can occur
 debconf-set-selections <<< "postfix postfix/mailname string $DOMAINNAME"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+
+
 
 # ------------------------------------------------------------
 log "Install all necessary packages"
@@ -131,6 +154,8 @@ APT::Periodic::AutocleanInterval "7";
 APT::Periodic::Unattended-Upgrade "1";
 EOT
 
+
+
 # ----------------
 # CONFIGURE NGINX
 # ----------------
@@ -138,15 +163,9 @@ EOT
 
 
 
-
-
-
 # ------------------------------------------------------------
 log "Allowing nginx through firewall"
 ufw allow 'Nginx Full'
-
-
-
 
 
 
@@ -158,9 +177,6 @@ mkdir -p /var/www/$DOMAINNAME/data
 mkdir -p /var/www/$DOMAINNAME/log
 chown -R $DEPLOYUSER:$DEPLOYUSER /var/www/$DOMAINNAME
 chmod -R 755 /var/www/$DOMAINNAME
-
-
-
 
 
 
@@ -264,26 +280,40 @@ server {
 }
 EOT
 
+
+
 # Active the server block
 log "Activating server block"
 ln -s /etc/nginx/sites-available/$DOMAINNAME /etc/nginx/sites-enabled/
 
+
+
 # https://gist.github.com/muhammadghazali/6c2b8c80d5528e3118613746e0041263
 # sed -i -e 's/# server_names_hash_bucket_size 64;/server_names_hash_bucket_size 64;/g' /etc/nginx/nginx.conf
+
+
 
 # Config okay with nginx?
 nginx -t
 
+
+
 log "Restarting nginx"
 systemctl restart nginx
+
+
 
 # Activate Certbot for this server block
 log "Adding certbot LetsEncrypt certificate"
 certbot --nginx --noninteractive -d $DOMAINALIASES_COMMA_SEPARATED --redirect --agree-tos -m $EMAILADDRESS
 
+
+
 # # Renew certbot certificates automatically
 log "Adding auto-renew for certbot"
 systemctl status certbot.timer
+
+
 
 # log "Restarting nginx"
 systemctl restart nginx
@@ -298,36 +328,30 @@ systemctl restart nginx
 # Use DigitalOcean droplet API to get the real public IP address
 echo "$(curl http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address) $DOMAINNAME $HOSTNAME" >> /etc/hosts
 
+
 echo "$HOSTNAME" >> /etc/hostname
+
 
 cat <<EOT > /etc/postfix/main.cf
 # See /usr/share/postfix/main.cf.dist for a commented, more complete version
 # myorigin=/etc/mailname
-
 smtpd_banner = \$myhostname ESMTP $mail_name (Ubuntu)
 biff = no
-
 # appending .domain is the MUA's job.
 append_dot_mydomain = no
-
 # Uncomment the next line to generate "delayed mail" warnings
 #delay_warning_time = 4h
-
 readme_directory = no
-
 # See http://www.postfix.org/COMPATIBILITY_README.html -- default to 2 on
 # fresh installs.
 compatibility_level = 2
-
 # TLS parameters
 # smtpd_tls_cert_file=/etc/letsencrypt/live/$DOMAINNAME/fullchain.pem
 # smtpd_tls_key_file=/etc/letsencrypt/live/$DOMAINNAME/privkey.pem
 smtpd_tls_security_level=may
-
 smtp_tls_CApath=/etc/ssl/certs
 smtp_tls_security_level=may
 smtp_tls_session_cache_database = btree:\${data_directory}/smtp_scache
-
 smtpd_relay_restrictions = permit_mynetworks permit_sasl_authenticated defer_unauth_destination
 # myhostname = /etc/hostname
 alias_maps = hash:/etc/aliases
@@ -341,9 +365,13 @@ inet_interfaces = loopback-only
 inet_protocols = all
 EOT
 
+
+
 # Forward all root email to $EMAILADDRESS
 echo "postmaster: $EMAILADDRESS" >> /etc/aliases
 newaliases
+
+
 
 # Restart Postfix
 systemctl restart postfix
@@ -359,9 +387,15 @@ systemctl restart postfix
 
 log "Setting up PM2 node"
 
+
+
 npm install pm2@latest -g
 
+
+
 env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u $DEPLOYUSER --hp /home/$DEPLOYUSER
+
+
 
 log "Switching to $SUDOUSER to activate pm2"
 su - $SUDOUSER
