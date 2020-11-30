138b9014f3bbeb873c101cbc5a5564ddd2ff2cf8
Charlie Owen <charlie@sonniesedge.co.uk>
2020-11-30 13:32:26 +0100

    Update actions.sh

--- actions.sh
+++ actions.sh
@@ -4,7 +4,7 @@
 # ------------------------------------------------------------
 HOSTNAME=whalecoiner
 DOMAINNAME=whalecoiner.com
-DOMAINALIASES=(www.whalecoiner.com whalecoiner.net www.whalecoiner.net whalecoiner.org www.whalecoiner.org timidra.in)
+DOMAINALIASES=(www.whalecoiner.com whalecoiner.net www.whalecoiner.net whalecoiner.org www.whalecoiner.org timidra.in sonniesedge.co.uk www.sonniesedge.co.uk)
 EMAILADDRESS=charlie@sonniesedge.co.uk
 LOG=/var/log/serversetup.log
 DOMAINALIASES_COMMA_SEPARATED=$(printf '%s,' "${DOMAINALIASES[@]}")
