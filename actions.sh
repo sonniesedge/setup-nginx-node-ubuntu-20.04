5caafca3a142ce672ac085dc1cfca1fe153921d7
Chazza <74922321+whalecoiner@users.noreply.github.com>
2021-04-06 18:02:40 +0200

    Update actions.sh

--- actions.sh
+++ actions.sh
@@ -133,7 +133,7 @@ debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Si
 # ------------------------------------------------------------
 log "Install all necessary packages"
 apt-add-repository universe
-apt-get -qqy install nginx certbot python3-certbot-nginx brotli build-essential libssl-dev mailutils whois unattended-upgrades nodejs 
+apt-get -qqy install nginx certbot python3-certbot-nginx brotli build-essential libssl-dev mailutils whois unattended-upgrades nodejs ca-certificates fonts-liberation libappindicator3-1 libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils
 
 # ------------------------------------------------------------
 log "Setting up unattended upgrades"
